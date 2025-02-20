fa6354837842952a01b39ec52685691799cf9c63
ArchitJain2134 <144250786+ArchitJain2134@users.noreply.github.com>
2025-02-20 15:15:13 +0530

    double rent

---
+++ doubleRent/main.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+	// your code goes here
+	var X int
+	fmt.Println("Enter the rent amount")
+	fmt.Scan(&X)
+	y:=2*X
+	fmt.Println(y)   // showing the amount chefina has to pay
+}
